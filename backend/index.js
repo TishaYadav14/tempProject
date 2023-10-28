318bffb62c06cb2a0ffc94ff0e8586ba0c9ebbde
TishaYadav14 <tyadhuvanshi1408@gmail.com>
2023-10-28 12:54:48 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+const dishes = require("./routes/dishRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+app.use(express.json());
+
+mongoose
+  // .connect("mongodb://localhost:27017/fooddelivery")
+  .connect("mongodb+srv://tyadav1408:iXcKGOKVCeYdpfA5@cluster0.h1qx7b4.mongodb.net/?retryWrites=true&w=majority")
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// middleware
+app.use((req, res, next) => {
+  console.log("Time:", Date.now());
+  next();
+});
+
+app.use("/api", dishes);
+app.use("/api/user", userRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
